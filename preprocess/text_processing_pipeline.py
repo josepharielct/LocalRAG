23a4822c4d1e616db36925eaf14ef64a2f123926
Joseph Ariel Christopher Teja <josephac.teja@gmail.com>
2024-04-22 16:20:39 +0800

    add initial files

---
+++ preprocess/text_processing_pipeline.py
@@ -0,0 +1,46 @@
+import fitz # (pymupdf, found this is better than pypdf for our use case, note: licence is AGPL-3.0, keep that in mind if you want to use any code commercially)
+from tqdm.auto import tqdm # for progress bars, requires !pip install tqdm
+from spacy.lang.en import English
+
+def text_formatter(text: str) -> str:
+    """Performs minor formatting on text."""
+    cleaned_text = text.replace("\n", " ").strip() # note: this might be different for each doc (best to experiment)
+
+    # Other potential text formatting functions can go here
+    return cleaned_text
+def split_to_sentences(sentence_list:list):
+    nlp = English()
+    nlp.add_pipe("sentencizer")
+    sentences = list(nlp(sentence_list).sents)
+    sentences = [str(sentence) for sentence in sentences]
+    sentence_count = len(sentences)
+    return sentences, sentence_count
+# Open PDF and get lines/pages
+# Note: this only focuses on text, rather than images/figures etc
+def open_and_read_pdf(pdf_path: str) -> list[dict]:
+    """
+    Opens a PDF file, reads its text content page by page, and collects statistics.
+
+    Parameters:
+        pdf_path (str): The file path to the PDF document to be opened and read.
+
+    Returns:
+        list[dict]: A list of dictionaries, each containing the page number
+        (adjusted), character count, word count, sentence count, token count, and the extracted text
+        for each page.
+    """
+    doc = fitz.open(pdf_path)  # open a document
+    pages_and_texts = []
+    for page_number, page in tqdm(enumerate(doc)):  # iterate the document pages
+        text = page.get_text()  # get plain text encoded as UTF-8
+        text = text_formatter(text)
+        pages_and_texts.append({"page_number": page_number - 41,  # adjust page numbers since our PDF starts on page 42
+                                "page_char_count": len(text),
+                                "page_word_count": len(text.split(" ")),
+                                "page_sentence_count_": len(text.split(". ")),
+                                "page_token_count": len(text) / 4,  # 1 token = ~4 chars, see: https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
+                                "text": text})
+    return pages_and_texts
+
+pages_and_texts = open_and_read_pdf(pdf_path=pdf_path)
+pages_and_texts[:2]
